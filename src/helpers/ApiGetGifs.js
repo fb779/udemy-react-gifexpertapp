7c078e499bde69eaa178488ade2159d35f44ccac
Fabian Forero <fabianforero779@gmail.com>
2020-12-13 19:38:51 -0500

    Ajuste ruta giphy https

--- src/helpers/ApiGetGifs.js
+++ src/helpers/ApiGetGifs.js
@@ -1,7 +1,7 @@
 import {api_key} from '../config/config';
 
 export const getGifs = async (category) => {
-  const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=${10}&api_key=${api_key}`;
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=${10}&api_key=${api_key}`;
   const resp = await fetch(url);
   const {data} = await resp.json();
   const gifs = data.map(({id, title, images: {downsized_large: {url}}}) => ({id, title, url}));
